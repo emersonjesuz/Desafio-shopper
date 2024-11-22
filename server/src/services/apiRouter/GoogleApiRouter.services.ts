df4376386b7735784555cc00d3ebb6f73d6130d9
emersonjesuz <josiemerson2013@gmail.com>
2024-11-22 09:42:28 -0300

    fix(server): Fix search error coming from Google Maps API

--- server/src/services/apiRouter/GoogleApiRouter.services.ts
+++ server/src/services/apiRouter/GoogleApiRouter.services.ts
@@ -1,11 +1,9 @@
-import { ServerError } from "../../helpers/Api-error";
+import { BadRequestError, ServerError } from "../../helpers/Api-error";
 import { ApiRouterService } from "./ApiRouter.services";
 import axios, { AxiosError } from "axios";
 
 export class GoogleApiRouterService implements ApiRouterService {
   async calculateRoute(destination: string, origin: string) {
-    console.log(destination, origin);
-
     const body = {
       origin: {
         address: origin,
@@ -16,20 +14,25 @@ export class GoogleApiRouterService implements ApiRouterService {
       travelMode: "DRIVE",
     };
 
-    const response = await axios
+    const { data } = await axios
       .post("https://routes.googleapis.com/directions/v2:computeRoutes", body, {
         headers: {
           "Content-Type": "application/json",
           "X-Goog-Api-Key": process.env.GOOGLE_API_KEY,
-          "X-Goog-FieldMask":
-            "routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline",
+          "X-Goog-FieldMask": "routes.legs",
         },
       })
       .catch((error: AxiosError) => {
         throw new ServerError(error.message);
       });
-    console.log(response);
 
-    return response.data;
+    if (!data.routes) {
+      throw new BadRequestError(
+        "Os dados fornecidos no corpo da requisição são inválidos",
+        "INVALID_DATA"
+      );
+    }
+
+    return data;
   }
 }
