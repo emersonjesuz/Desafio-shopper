b8cd88deca6c3b6d4dc84179346f46a09bb80d9d
emersonjesuz <josiemerson2013@gmail.com>
2024-11-23 17:50:15 -0300

    feat(server): add  controller

--- server/src/controllers/Estimate.controllers.ts
+++ server/src/controllers/Estimate.controllers.ts
@@ -1,15 +1,39 @@
 import { Response, Request } from "express";
 import { ApiRouterService } from "../services/apiRouter/ApiRouter.services";
+import { EstimateUseCases } from "../useCases/Estimate.useCases";
+import { ImMemoryRepositories } from "../database/repositories/InMemory.repositories";
+import { DistanceFormatterKilometersMinute } from "../utils/distanceFormatter/DistanceFormatterkilometersMinute.utils";
+import { EstimateCreateResponseDTO } from "../dtos/EstimateCreateResponse.dtos";
+import { TimeFormatterMinuteOrHours } from "../utils/timeFormatter.ts/TimeFormatterMinuteOrHours.utils";
 
 export class EstimateController {
+  private estimateCreateUseCases = new EstimateUseCases(
+    new ImMemoryRepositories()
+  );
   constructor(private readonly apiRouterService: ApiRouterService) {}
   public async create(request: Request, response: Response): Promise<void> {
-    const routes = await this.apiRouterService.calculateRoute(
-      "macurure",
-      "salvador"
+    const { origin, destination } = request.body;
+
+    const data = await this.apiRouterService.calculateRoute(
+      origin,
+      destination
     );
-    response.status(200).json({
-      routes,
-    });
+
+    const distanceFormatterKilometersMinute =
+      new DistanceFormatterKilometersMinute();
+    const kilometer = distanceFormatterKilometersMinute.format(data.distance);
+    const timeFormatterMinuteOrHours = new TimeFormatterMinuteOrHours();
+    const drivers = await this.estimateCreateUseCases.list(kilometer, "asc");
+
+    const output: EstimateCreateResponseDTO = {
+      origin: data.origin,
+      destination: data.destination,
+      distance: data.distance,
+      duration: timeFormatterMinuteOrHours.format(data.duration),
+      options: drivers,
+      routeResponse: data.routeResponse,
+    };
+
+    response.status(200).json({ ...output });
   }
 }
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,10 +1,16 @@
 import { Router, Request, Response } from "express";
 import { EstimateController } from "./controllers/Estimate.controllers";
+import { EstimateCreateMiddlewares } from "./middlewares/estimate/EstimateCreate.middlewares";
 import { GoogleApiRouterService } from "./services/apiRouter/GoogleApiRouter.services";
 
 export const router = Router();
 const estimateController = new EstimateController(new GoogleApiRouterService());
+const estimateCreateMiddlewares = new EstimateCreateMiddlewares();
 
-router.post("/ride/estimate", (req, res) =>
-  estimateController.create(req, res)
+router.post(
+  "/ride/estimate",
+  estimateCreateMiddlewares.execute,
+  (request: Request, response: Response) => {
+    estimateController.create(request, response);
+  }
 );
