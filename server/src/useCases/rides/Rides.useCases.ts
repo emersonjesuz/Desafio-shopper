71462b65120c9fff924b0a883faa3c06d7ca5c85
emersonjesuz <josiemerson2013@gmail.com>
2024-11-24 15:06:42 -0300

    feat(server): Add rides list in use cases

--- server/src/useCases/rides/Rides.useCases.ts
+++ server/src/useCases/rides/Rides.useCases.ts
@@ -1,5 +1,13 @@
 import { RidesCreateDto } from "../../dtos/RidesCreateDto.dtos";
-import { NotAcceptableError, NotFoundError } from "../../helpers/Api-error";
+import {
+  RidesListResponseDto,
+  RideResponse,
+} from "../../dtos/RidesListResponse.dtos";
+import {
+  BadRequestError,
+  NotAcceptableError,
+  NotFoundError,
+} from "../../helpers/Api-error";
 import { DriversRepository } from "../../repositories/drivers/Drivers.repositories";
 import { RidesRepository } from "../../repositories/rides/Rides.repositories";
 import { DistanceFormatterKilometersMinute } from "../../utils/distanceFormatter/DistanceFormatterkilometersMinute.utils";
@@ -31,5 +39,49 @@ export class RidesUseCases {
       );
 
     return await this.ridesRepository.create(ridesCreateDto);
+  }
+
+  async list(
+    customer_id: string,
+    driver_id?: number
+  ): Promise<RidesListResponseDto> {
+    const isThereCustomer = await this.ridesRepository.findByCustomerId(
+      customer_id
+    );
+
+    if (!isThereCustomer)
+      throw new NotFoundError("Nenhum registro encontrado", "NO_RIDES_FOUND");
+
+    if (driver_id) {
+      const isThereDriver = await this.driversRepository.findById(driver_id);
+
+      if (!isThereDriver)
+        throw new BadRequestError("Motorista invalido", "INVALID_DRIVER");
+    }
+
+    const rides = await this.ridesRepository.list(customer_id, driver_id);
+
+    const ridesFormattedResponse: RideResponse[] = rides.map((ride) => {
+      return {
+        id: ride.id,
+        date: ride.date,
+        origin: ride.origin,
+        destination: ride.destination,
+        distance: ride.distance,
+        duration: ride.duration,
+        driver: {
+          id: ride.driver.id,
+          name: ride.driver.name,
+        },
+        value: ride.value,
+      };
+    });
+
+    const ridesResponse: RidesListResponseDto = {
+      customer_id,
+      rides: ridesFormattedResponse,
+    };
+
+    return ridesResponse;
   }
 }
