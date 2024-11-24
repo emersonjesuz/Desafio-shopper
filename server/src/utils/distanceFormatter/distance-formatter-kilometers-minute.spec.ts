4757ac483d248698b60a346264e1059db25e2079
emersonjesuz <josiemerson2013@gmail.com>
2024-11-24 12:03:07 -0300

    test(server): Add expect

--- server/src/utils/distanceFormatter/distance-formatter-kilometers-minute.spec.ts
+++ server/src/utils/distanceFormatter/distance-formatter-kilometers-minute.spec.ts
@@ -5,7 +5,9 @@ describe("Distance formatter kilometers minute", () => {
   it("Should return correct formats", () => {
     const distanceFormatter = new DistanceFormatterKilometersMinute();
     const formatterDistance0Km = distanceFormatter.format(0);
+    const formatterDistance10Km = distanceFormatter.format(10000);
 
     expect(formatterDistance0Km).toEqual(0);
+    expect(formatterDistance10Km).toEqual(10);
   });
 });
