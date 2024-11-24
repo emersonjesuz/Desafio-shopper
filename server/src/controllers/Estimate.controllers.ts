3611c5855de2f8daa3ac63970b5bc7e08307d74f
emersonjesuz <josiemerson2013@gmail.com>
2024-11-24 09:01:17 -0300

    feat(server): Add Prisma services

--- server/src/controllers/Estimate.controllers.ts
+++ server/src/controllers/Estimate.controllers.ts
@@ -1,14 +1,14 @@
-import { Response, Request } from "express";
+import { Request, Response } from "express";
+import { EstimateCreateResponseDTO } from "../dtos/EstimateCreateResponse.dtos";
+import { PrismaDriversRepositories } from "../repositories/drivers/PrismaDrivers.repositories";
 import { ApiRouterService } from "../services/apiRouter/ApiRouter.services";
 import { EstimateUseCases } from "../useCases/Estimate.useCases";
-import { ImMemoryRepositories } from "../database/repositories/InMemory.repositories";
 import { DistanceFormatterKilometersMinute } from "../utils/distanceFormatter/DistanceFormatterkilometersMinute.utils";
-import { EstimateCreateResponseDTO } from "../dtos/EstimateCreateResponse.dtos";
 import { TimeFormatterMinuteOrHours } from "../utils/timeFormatter.ts/TimeFormatterMinuteOrHours.utils";
 
 export class EstimateController {
   private estimateCreateUseCases = new EstimateUseCases(
-    new ImMemoryRepositories()
+    new PrismaDriversRepositories()
   );
   constructor(private readonly apiRouterService: ApiRouterService) {}
   public async create(request: Request, response: Response): Promise<void> {
