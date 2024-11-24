ab548ec1a3c3f53d811987bf56e2a4984b776b00
emersonjesuz <josiemerson2013@gmail.com>
2024-11-24 12:16:03 -0300

    feat(server): Add rides controller

---
+++ server/src/controllers/Rides.controllers.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { RidesCreateDto } from "../dtos/RidesCreateDto.dtos";
+import { RidesUseCases } from "../useCases/rides/Rides.useCases";
+import { PrismaRidesRepository } from "../repositories/rides/PrismaRides.repositories";
+import { PrismaDriversRepositories } from "../repositories/drivers/PrismaDrivers.repositories";
+export class RidesController {
+  private readonly ridesUseCases = new RidesUseCases(
+    new PrismaRidesRepository(),
+    new PrismaDriversRepositories()
+  );
+  async save(request: Request, response: Response): Promise<void> {
+    const {
+      customer_id,
+      destination,
+      distance,
+      driver,
+      duration,
+      origin,
+      value,
+    } = request.body as unknown as RidesCreateDto;
+
+    const save = await this.ridesUseCases.create({
+      customer_id,
+      destination,
+      distance,
+      driver,
+      duration,
+      origin,
+      value,
+    });
+
+    response.status(200).json({ success: save });
+  }
+}
--- server/src/router.ts
+++ server/src/router.ts
@@ -2,15 +2,27 @@ import { Router, Request, Response } from "express";
 import { EstimateController } from "./controllers/Estimate.controllers";
 import { EstimateCreateMiddlewares } from "./middlewares/estimate/EstimateCreate.middlewares";
 import { GoogleApiRouterService } from "./services/apiRouter/GoogleApiRouter.services";
+import { RidesCreateMiddleware } from "./middlewares/rides/RidesCreate.middleware";
+import { RidesController } from "./controllers/Rides.controllers";
 
 export const router = Router();
 const estimateController = new EstimateController(new GoogleApiRouterService());
 const estimateCreateMiddlewares = new EstimateCreateMiddlewares();
+const ridesController = new RidesController();
+const ridesCreateMiddleware = new RidesCreateMiddleware();
 
 router.post(
   "/ride/estimate",
   estimateCreateMiddlewares.execute,
-  (request: Request, response: Response) => {
-    estimateController.create(request, response);
+  async (request: Request, response: Response) => {
+    await estimateController.create(request, response);
+  }
+);
+
+router.patch(
+  "/ride/confirm",
+  ridesCreateMiddleware.execute,
+  async (request: Request, response: Response) => {
+    await ridesController.save(request, response);
   }
 );
