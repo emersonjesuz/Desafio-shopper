f42adf6e4dc6442ae7aaf348e9b427e1817853c6
emersonjesuz <josiemerson2013@gmail.com>
2024-11-25 17:02:07 -0300

    fix(web): Refactor file name

--- web/src/components/estimate/index.tsx
+++ web/src/components/estimate/index.tsx
@@ -4,7 +4,7 @@ import { EstimateForm } from "./EstimateForm";
 
 export default function EstimateScreen() {
   return (
-    <div className="flex w-full flex-col items-center justify-center">
+    <div className="flex w-[500px] flex-col items-center justify-center py-10 lg:min-h-[70vh]">
       <Image
         className="lg-h-auto h-[100px] w-1/2 object-cover lg:max-w-[400px]"
         src={carImage}
