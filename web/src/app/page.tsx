38afec6c7fddf34b1b85e87bb50105e50f30e656
emersonjesuz <josiemerson2013@gmail.com>
2024-11-26 16:18:35 -0300

    feat(web): Add page router

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,33 @@
-import EstimateScreen from "@/components/estimate/EstimateScreen";
+"use client";
+import { ErrorScreen } from "@/components/ErrorScreen";
+import EstimateScreen from "@/components/estimate";
+import { HistoricToursScreen } from "@/components/historicTours";
+import { LayoutScreen } from "@/components/LayoutScreen";
+import { SelectDriversScreen } from "@/components/selectDrivers/index";
+import { useErrorStore } from "@/stores/useErrorStore";
+import { useRouterStore } from "@/stores/useRouterStore";
 
 export default function Home() {
-  return <EstimateScreen />;
+  const { router } = useRouterStore((state) => state);
+  const { error } = useErrorStore((state) => state);
+
+  function RenderScreen() {
+    switch (router) {
+      case "estimate":
+        return <EstimateScreen />;
+      case "drivers":
+        return <SelectDriversScreen />;
+      case "rides":
+        return <HistoricToursScreen />;
+      default:
+        return null;
+    }
+  }
+
+  return (
+    <LayoutScreen>
+      <RenderScreen />
+      {error.show && <ErrorScreen />}
+    </LayoutScreen>
+  );
 }
